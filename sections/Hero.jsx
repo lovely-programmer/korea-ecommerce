3644bf425afd3add0a1837499c946574fa044d22
lovely-programmer <olaoluwaolasunkanmi5@gmail.com>
2025-08-25 18:08:20 +0100

    update hero.jsx

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -3,7 +3,16 @@ import "./styles.css";
 export default function Hero() {
   return (
     <div className="hero">
-      <div className="hero-image"></div>
+      <div className="hero-image">
+        <div className="hero-text">
+          <h1>Spend $150 To Become Member Get 2000% Membership points</h1>
+          <p className="text-sm">
+            The membership will be valid from first purchase the point will be
+            credited to your account after shipment
+          </p>
+          <button>Start Shopping</button>
+        </div>
+      </div>
     </div>
   );
 }
